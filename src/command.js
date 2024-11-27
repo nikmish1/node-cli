d987fc0c2280046cfd8c2dd63de1372bba263485
Nikhil Mishra <nikhil15.1990@gmail.com>
2024-11-27 20:28:27 +0530

    Implement note-taking functionality with CRUD operations and add Jest for testing

--- src/command.js
+++ src/command.js
@@ -1,5 +1,21 @@
 import yargs from 'yargs'
 import { hideBin } from 'yargs/helpers'
+import { findNotes, getAllNotes, newNote, removeAllNotes, removeNote } from './notes.js'
+
+// should be in utils.js
+
+const listNotes = (notes) => {
+    console.log("Notes: ");
+
+    notes.forEach(({ id, content, tags }) => {
+        console.log(`ID: ${id} - ${content}`);
+        if (tags.length) {
+            console.log(`Tags: ${tags.join(', ')}`);
+        }
+
+        console.log("\n");
+    })
+}
 
 yargs(hideBin(process.argv))
     .command('new <note>', 'Create a new note', yargs => {
@@ -7,15 +23,19 @@ yargs(hideBin(process.argv))
             description: 'the content of note to create',
             type: 'string'
         })
-    }, (argv) => {
-        console.log("Creating a new note with content:", argv.note)
+    }, async (argv) => {
+        const tags = argv.tags ? argv.tags?.split(',') : [];
+
+        const note = await newNote(argv.note, tags);
+        console.log("New Note Created: ", note);
     }).option('tags', {
         alias: 't',
-        type: 'array',
+        type: 'string',
         description: 'add tags to the note'
     })
     .command('all', 'get all notes', () => { }, async (argv) => {
-
+        const notes = await getAllNotes();
+        listNotes(notes);
     })
     .command('find <filter>', 'get matching notes', yargs => {
         return yargs.positional('filter', {
@@ -23,7 +43,8 @@ yargs(hideBin(process.argv))
             type: 'string'
         })
     }, async (argv) => {
-
+        const notes = await findNotes(argv.filter);
+        listNotes(notes);
     })
     .command('remove <id>', 'remove a note by id', yargs => {
         return yargs.positional('id', {
@@ -31,6 +52,12 @@ yargs(hideBin(process.argv))
             description: 'The id of the note you want to remove'
         })
     }, async (argv) => {
+        try {
+            await removeNote(argv.id);
+            console.log(`Note with id ${argv.id} removed`);
+        } catch (e) {
+            console.error(e.message);
+        }
 
     })
     .command('web [port]', 'launch website to see notes', yargs => {
@@ -44,7 +71,8 @@ yargs(hideBin(process.argv))
 
     })
     .command('clean', 'remove all notes', () => { }, async (argv) => {
-
+        await removeAllNotes();
+        console.log("All notes removed");
     })
     .demandCommand(1)
     .parse()
